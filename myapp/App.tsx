c053a1a08b191589a6c24b6fc183f3b1193e5ffc
Kritchanaxt <145214064+Kritchanaxt@users.noreply.github.com>
2025-07-06 01:17:20 +0700

    update

--- myapp/App.tsx
+++ myapp/App.tsx
@@ -1,17 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, type ViewStyle } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Hello max </Text>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello khon E3 got </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+interface Styles {
+  container: ViewStyle;
+}
+
+const styles = StyleSheet.create<Styles>({
   container: {
     flex: 1,
     backgroundColor: '#ffff',
